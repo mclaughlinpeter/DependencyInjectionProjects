0926377a20c327dafb6207ec4b85d26b57a4e2dc
mclaughlinpeter <p.f.mclaughlin@hotmail.co.uk>
2017-12-26 17:39:31 -0500

    Add IRepository property to HomeController.cs to allow Mocking; Add DITests.cs with ControllerTest() unit test which Mocks the IRepository and asserts that model data is correctly passed to the view

--- DependencyInjection/Controllers/HomeController.cs
+++ DependencyInjection/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ namespace DependencyInjection.Controllers
 {
     public class HomeController : Controller
     {
-        public ViewResult Index() => View(new MemoryRepository().Products);
+        public IRepository Repository { get; set; } = new MemoryRepository();
+        
+        public ViewResult Index() => View(Repository.Products);
     }
 }

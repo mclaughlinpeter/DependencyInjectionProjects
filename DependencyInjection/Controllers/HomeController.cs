2fdd339cd721d4438e8a0fac00315ddb7abe8e1b
mclaughlinpeter <p.f.mclaughlin@hotmail.co.uk>
2017-12-26 19:49:32 -0500

    Modify HomeController to use TypeBroker and modify corresponding unit test

--- DependencyInjection/Controllers/HomeController.cs
+++ DependencyInjection/Controllers/HomeController.cs
@@ -1,11 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using DependencyInjection.Models;
+using DependencyInjection.Infrastructure;
 
 namespace DependencyInjection.Controllers
 {
     public class HomeController : Controller
     {
-        public IRepository Repository { get; set; } = new MemoryRepository();
+        public IRepository Repository { get; } = TypeBroker.Repository;
         
         public ViewResult Index() => View(Repository.Products);
     }

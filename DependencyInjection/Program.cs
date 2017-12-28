8f74a9990d02f1956624db13e3ef4683422d7b2a
mclaughlinpeter <p.f.mclaughlin@hotmail.co.uk>
2017-12-27 19:11:23 -0500

    Modify Startup class to take a IHostingEnvironment argument in the ctor and use it in the ConfigureServices method to select different IRepository implementations depending on the environment; set environment to development in the Program class

--- DependencyInjection/Program.cs
+++ DependencyInjection/Program.cs
@@ -20,6 +20,7 @@ namespace DependencyInjection
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseEnvironment("Development")
                 .Build();
     }
 }
